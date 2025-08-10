ffc76719faba76c2c73eb6d91ea7ae98c3af8737
paulie-of-punskas <pbernecki@protonmail.com>
2025-08-10 10:07:15 +0200

    added PNG file

--- png/png_test.go
+++ png/png_test.go
@@ -4,7 +4,6 @@ import (
 	// "encoding/binary"
 	// "io"
 
-	// "github.com/evanoberholster/imagemeta/imagetype"
 	// "github.com/evanoberholster/imagemeta/meta"
 	// "github.com/evanoberholster/imagemeta/meta/utils"
 
@@ -21,29 +20,29 @@ var (
 		{"PNG_boy-217417_1280.png", true},
 		{"PNG_fruit-1234657_1280.png", true},
 		{"PNG_pub-2992808_1280.png", true},
+		{"basn3p08.png", true},
 	}
 )
 
 func TestScanPNG(t *testing.T) {
+	t.Run("Read PNG files, expect no Exif", func(t *testing.T) {
+		want := [2]string{"ByteOrder: UnknownEndian, Ifd: UnknownIfd, Offset: 0x0000 TiffOffset: 0x0000 Length: 0 Imagetype: application/octet-stream", "error no Exif"}
 
-	want := [2]string{"ByteOrder: UnknownEndian, Ifd: UnknownIfd, Offset: 0x0000 TiffOffset: 0x0000 Length: 0 Imagetype: application/octet-stream", "error no Exif"}
+		for j := 0; j < len(benchmarksPNG); j++ {
+			file, err := os.Open(dir + benchmarksPNG[j].fileName)
+			defer file.Close()
+			if err != nil {
+				t.Fatal("Error while reading:", err)
+			}
+			head, meta := ScanPngHeader(file)
 
-	for j := 0; j < len(benchmarksPNG); j++ {
+			if want[0] != head.String() {
+				t.Fatalf("Header is different for %s: wanted %s, got %s", benchmarksPNG[j].fileName, want, head)
+			}
 
-		file, err := os.Open(dir + benchmarksPNG[j].fileName)
-		if err != nil {
-			t.Fatal("Error while reading:", err)
+			if want[1] != meta.Error() {
+				t.Fatalf("Error message differs for %s: wanted %s, got %s", benchmarksPNG[j].fileName, want, meta)
+			}
 		}
-		defer file.Close()
-		head, meta := ScanPngHeader(file)
-
-		if want[0] != head.String() {
-			t.Fatalf("Header is different for %s : want: %s, got: %s", benchmarksPNG[j].fileName, want, head)
-		}
-
-		if want[1] != meta.Error() {
-			t.Fatalf("Error message differs for %s: want: %s, got: %s", benchmarksPNG[j].fileName, want, meta)
-		}
-
-	}
+	})
 }
